150f5b68dc1e1f43f471abdd6daf5b4225404b20
Anvar Kildebekov <fedoranvar@yandex.ru>
2024-03-20 23:59:38 +0500

    upd

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,3 +1,15 @@
 <template>
-<img  id='logo' src="./logo_docs.png">
+  <p class="inline-flex mt-2  opacity-50">
+    <div class="uppercase scale-y-150  mt-2 -tracking-0.2 text-grey-900 text-2xl">
+    Адванс
+    </div>
+
+<div>
+    <img  id='logo' src="./logo_docs.png" class='ml-2 w-10'>
+    <div class="text-blue-700 ml-2 tracking-1">
+    докс 
+    </div>
+    </div>
+
+  </p>
 </template>
